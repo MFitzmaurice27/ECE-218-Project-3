57e60b83246d5ab9a01d9b3da5332254b2ef0f36
MFitzmaurice27 <fitzmaum@union.edu>
2025-01-15 14:48:37 -0500

    Removed uartTask() function from main.cpp

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main()
     outputsInit();
     while (true) {
         
-        uartTask();
+        
     }
 }
 
@@ -60,7 +60,4 @@ void outputsInit()
     driverSeatbelt.mode(PullDown);
 }
 
-void uartTask()
-{
-    
-}
+
