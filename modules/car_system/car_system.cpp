4fc7e780b8e18b2441453871b338f82831781c16
MFitzmaurice27 <fitzmaum@union.edu>
2025-02-12 14:39:52 -0500

    Added module files

---
+++ modules/car_system/car_system.cpp
@@ -0,0 +1,17 @@
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+
+//=====[Declaration of private defines]========================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of private global variables]============
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
---
+++ modules/car_system/car_system.h
@@ -0,0 +1,14 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _CAR_SYSTEM_H_
+#define _CAR_SYSTEM_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//=====[#include guards - end]=================================================
+
+#endif // _CAR_SYSTEM_H_
