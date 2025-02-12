a8796a56692110be75536fa983c2504f46cf5cc5
MFitzmaurice27 <fitzmaum@union.edu>
2025-02-12 15:08:40 -0500

    Added modules folder

---
+++ modules/buzzer/buzzer.h
@@ -0,0 +1,18 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _BUZZER_H_
+#define _BUZZER_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void BuzzerInit();
+void BuzzerOn();
+void BuzzerOff();
+
+//=====[#include guards - end]=================================================
+
+#endif // _BUZZER_H_
---
+++ modules/ignition/ignition.h
@@ -0,0 +1,15 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _IGNITION_H
+#define _IGNITION_H
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+//=====[#include guards - end]=================================================
+
+#endif // _IGNITION_H
